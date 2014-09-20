bafea5e23b4f375baa8021e0e40a4f0d04013ba9
DonCream <lamar.mcmiller@gmail.com>
2014-09-19 23:10:16 -0400

    MaxLengthOnNames migration timestamp added
    moved instruction to ensure the project would  build p.129

--- ContosoUniversityCF/ContosoUniversityCF/Migrations/Configuration.cs
+++ ContosoUniversityCF/ContosoUniversityCF/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace ContosoUniversityCF.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(ContosoUniversityCF.Models.ApplicationDbContext context)
